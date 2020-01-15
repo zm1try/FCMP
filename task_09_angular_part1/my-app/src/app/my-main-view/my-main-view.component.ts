3bf4f4483688194d738749d55e5649b6b3cd715e
Dzmitry Yarash <Dzmitry_Yarash@epam.com>
2020-01-15 17:39:27 +0300

    feat: added required functionality

---
+++ task_09_angular_part1/my-app/src/app/my-main-view/my-main-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-main-view',
+  templateUrl: './my-main-view.component.html',
+  styleUrls: ['./my-main-view.component.css']
+})
+export class MyMainViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
