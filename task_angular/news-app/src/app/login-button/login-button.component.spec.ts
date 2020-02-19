b1b511197ba02bd986ed7404b17ed8458029ed47
zm1try <zmitryarosh@gmail.com>
2020-02-20 01:54:16 +0300

    feat: added additional unit tests

--- task_angular/news-app/src/app/login-button/login-button.component.spec.ts
+++ task_angular/news-app/src/app/login-button/login-button.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { UserService } from '../user.service';
 import { LoginButtonComponent } from './login-button.component';
 
 describe('LoginButtonComponent', () => {
   let component: LoginButtonComponent;
   let fixture: ComponentFixture<LoginButtonComponent>;
 
+  const userMock = {
+    getCurrentUserName: () => {},
+    logOut: () => {},
+    logIn: () => {}
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginButtonComponent ]
+      declarations: [ LoginButtonComponent ],
+      providers: [
+        { provide: UserService, useValue: userMock }
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +25,41 @@ describe('LoginButtonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginButtonComponent);
     component = fixture.componentInstance;
-    // fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should call methods', () => {
+      spyOn<any>(component['user'], 'getCurrentUserName');
+      component.ngOnInit();
+      expect(component['user'].getCurrentUserName).toHaveBeenCalled();
+    });
+  });
+
+  describe('onClick', () => {
+    it('should call methods 1', () => {
+      spyOn<any>(component['user'], 'getCurrentUserName');
+      spyOn<any>(component['user'], 'logOut');
+      spyOn<any>(component['user'], 'logIn');
+      component.currentUser = null;
+      component.onClick();
+      expect(component['user'].logIn).toHaveBeenCalled();
+      expect(component['user'].logOut).not.toHaveBeenCalled();
+      expect(component['user'].getCurrentUserName).toHaveBeenCalled();
+    });
+
+    it('should call methods 2', () => {
+      spyOn<any>(component['user'], 'getCurrentUserName');
+      spyOn<any>(component['user'], 'logOut');
+      spyOn<any>(component['user'], 'logIn');
+      component.currentUser = 'some name';
+      component.onClick();
+      expect(component['user'].logIn).not.toHaveBeenCalled();
+      expect(component['user'].logOut).toHaveBeenCalled();
+      expect(component['user'].getCurrentUserName).toHaveBeenCalled();
+    });
   });
 });
--- task_angular/news-app/src/app/main-page/main-page.component.spec.ts
+++ task_angular/news-app/src/app/main-page/main-page.component.spec.ts
@@ -1,5 +1,7 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { NewsApiService } from '../news-api.service';
+import { LocalNewsService } from '../local-news.service';
+import { CurrentNewsListService } from '../current-news-list.service';
 import { MainPageComponent } from './main-page.component';
 import { FilterByWordPipe } from '../filter-by-word.pipe';
 
@@ -8,7 +10,16 @@ describe('MainPageComponent', () => {
   let fixture: ComponentFixture<MainPageComponent>;
 
   const newsApiMock = {
-    getSources: () => {}
+    getSources: () => {},
+    getItems: () => {}
+  }
+
+  const localNewsMock = {
+    getLocalArticles: () => {}
+  }
+
+  const currentNewsListMock = {
+    saveList: () => {}
   }
 
   beforeEach(async(() => {
@@ -16,6 +27,8 @@ describe('MainPageComponent', () => {
       declarations: [ MainPageComponent, FilterByWordPipe ],
       providers: [
         { provide: NewsApiService, useValue: newsApiMock },
+        { provide: LocalNewsService, useValue: localNewsMock },
+        { provide: CurrentNewsListService, useValue: currentNewsListMock },
       ]
     })
     .compileComponents();
@@ -29,5 +42,75 @@ describe('MainPageComponent', () => {
   it('should create', () => {
     // spyOn<any>(component['newsApi'], 'getSources').and.returnValue(Promise.resolve({}));
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should call methods', () => {
+      spyOn<any>(component['localNews'], 'getLocalArticles').and.returnValue([{some: 'some'}]);
+      spyOn<any>(component['currentListNews'], 'saveList');
+      spyOn<any>(component, 'getNewsSources');
+      component.ngOnInit();
+      expect(component['localNews'].getLocalArticles).toHaveBeenCalled();
+      expect(component['currentListNews'].saveList).toHaveBeenCalledWith([{some: 'some'}]);
+      expect(component.getNewsSources).toHaveBeenCalled();
+    });
+  });
+
+  describe('getNewsSources', () => {
+    it('should init sourcesList from response', fakeAsync(() => {
+      spyOn<any>(component['newsApi'], 'getSources').and.returnValue(Promise.resolve({sources: [{title: 'title'}]}));
+      component.getNewsSources();
+      tick();
+      expect(component['newsApi'].getSources).toHaveBeenCalled();
+      expect(component.sourcesList).toEqual( [{title: 'title'}]);
+    }));
+
+    it('should not init sourcesList from response', fakeAsync(() => {
+      spyOn<any>(component['newsApi'], 'getSources').and.returnValue(Promise.reject({}));
+      component.getNewsSources();
+      tick();
+      expect(component['newsApi'].getSources).toHaveBeenCalled();
+      expect(component.sourcesList).toEqual([]);
+    }));
+  });
+
+  describe('getNewsArticles', () => {
+    it('should call methods', fakeAsync(() => {
+      spyOn<any>(component['newsApi'], 'getItems').and.returnValue(Promise.resolve({articles: []}));
+      spyOn<any>(component['currentListNews'], 'saveList');
+      component.getNewsArticles({id: 'id', name: 'name'});
+      tick();
+      expect(component['newsApi'].getItems).toHaveBeenCalled();
+      expect(component['currentListNews'].saveList).toHaveBeenCalled();
+    }));
+
+    it('should not call method', fakeAsync(() => {
+      spyOn<any>(component['newsApi'], 'getItems').and.returnValue(Promise.reject({}));
+      spyOn<any>(component['currentListNews'], 'saveList');
+      component.getNewsArticles({id: 'id', name: 'name'});
+      tick();
+      expect(component['currentListNews'].saveList).not.toHaveBeenCalled();
+    }));
+  });
+
+  describe('filterListByCheckbox', () => {
+    it('should set list 1', () => {
+      spyOn<any>(component['currentListNews'], 'saveList');
+      component.localArticlesList = [{title: 'local'}];
+      component.isOnlyLocal = true;
+      component.filterListByCheckbox();
+      expect(component['currentListNews'].saveList).toHaveBeenCalledWith([{title: 'local'}]);
+      expect(component.listToDisplay).toEqual([{title: 'local'}]);
+    });
+
+    it('should set list 2', () => {
+      spyOn<any>(component['currentListNews'], 'saveList');
+      component.localArticlesList = [{title: 'local'}];
+      component.articlesList = [{title: 'local2'}];
+      component.isOnlyLocal = false;
+      component.filterListByCheckbox();
+      expect(component['currentListNews'].saveList).toHaveBeenCalledWith([{title: 'local'}, {title: 'local2'}]);
+      expect(component.listToDisplay).toEqual([{title: 'local'}, {title: 'local2'}]);
+    });
   });
 });
